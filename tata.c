96576ff3679383519864d2808219ce26faaeefdb
AkashBasak <112407537+AkashBasak@users.noreply.github.com>
2022-09-11 13:34:08 +0600

    Update tata.c

--- tata.c
+++ tata.c
@@ -3,6 +3,7 @@ struct process{
     int all;
     int max;
     int need;
+    int avail;
 };
 
 int main(){
@@ -10,7 +11,16 @@ int main(){
     int n,i;
     printf("Enter the nuber of process\n");
     scanf("%d",&n);
-    for(i=0;i<n;i++){
+     printf("Enter the number of allocated resource of 1st process\n");
+        scanf("%d",&myprocess[0].all);
+         printf("Enter the number of Maximum resource of 1st process\n");
+        scanf("%d",&myprocess[0].max);
+         printf("Enter the number of available resource of 1st process\n");
+        scanf("%d",&myprocess[0].avail);
+        myprocess[0].need= myprocess[0].max - myprocess[0].all;
+        
+    
+    for(i=1;i<n;i++){
         printf("Enter the number of allocated resource\n");
         scanf("%d",&myprocess[i].all);
          printf("Enter the number of Maximum resource\n");
@@ -19,15 +29,23 @@ int main(){
 
 
     }
+    for(i=1;i<n;i++){
+       if(myprocess[i-1].avail > myprocess[i].need){
+           myprocess[i].avail= myprocess[i].need + myprocess[i].all;
+       }
 
+    }
+    
+printf("allocated\tmax\tneed\tavailable");
+printf("\n");
 for(i=0;i<n;i++){
-        printf("allocated: %d\n", myprocess[i].all);
+        printf("%d\t", myprocess[i].all);
+         printf("%d\t",myprocess[i].max);
+         printf("%d\t",myprocess[i].need);
+         printf("%d\t",myprocess[i].avail);
 
-         printf("maximum need: %d\n",myprocess[i].max);
-         printf("remaining need: %d\n",myprocess[i].need);
-
-        myprocess[i].need= myprocess[i].max - myprocess[i].all;
-
+       
+        printf("\n");
 
     }
     return 0;
